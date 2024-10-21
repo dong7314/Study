8569ad00dd299f578e784a48bd6357d37ff6b173
LeeDongYeop <eaea7314@naver.com>
2024-10-21 23:22:23 +0900

    [add]: 7강 마무리

---
+++ Next/7강. 캐싱 이해 및 구성하기/01-starting-project/app/messages/page.js
@@ -0,0 +1,19 @@
+import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+
+export default async function MessagesPage() {
+  // unstable_noStore();
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: { tags: ["msg"] },
+  // });
+  // revalidateTag('msg')
+  // const messages = await response.json();
+
+  const messages = await getMessages();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
